e9306916b7f4ff088a06f096cda4e15da7f6dad3
Nico <n.rossi.studio@gmail.com>
2025-12-09 09:43:05 +0000

    Added UI Icons in the menu buttons

--- src/components/RecipeTimer.jsx
+++ src/components/RecipeTimer.jsx
@@ -5,15 +5,14 @@ import { useState, useEffect } from "react";
 //Tipologia toggle (espande e contrae il componente in base alla necessità dell'utente)
 
 function RecipeTimer({ totalMinutes }) {
-    
-    //Verifico che sia disponibile il tempo di preparazione
-    const hasTimer = totalMinutes && totalMinutes > 0;
-    const [isExpanded, setIsExpanded] = useState(false);
-    const [timerMode, setTimerMode] = useState('stopwatch'); //Utilizza sempre il cronometro di default
-    const [seconds, setSeconds] = useState(0);
-    const [isRunning, setIsRunning] = useState(false);
+  //Verifico che sia disponibile il tempo di preparazione
+  const hasTimer = totalMinutes && totalMinutes > 0;
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [timerMode, setTimerMode] = useState("stopwatch"); //Utilizza sempre il cronometro di default
+  const [seconds, setSeconds] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
 
-    //Funzione helper per formattare i secondi in MM:SS
+  //Funzione helper per formattare i secondi in MM:SS
   function formatTime(totalSeconds) {
     // 1. CALCOLO DEI MINUTI
     // Math.floor() arrotonda per difetto (elimina i decimali)
@@ -34,38 +33,38 @@ function RecipeTimer({ totalMinutes }) {
     return `${String(mins).padStart(2, "0")}:${String(secs).padStart(2, "0")}`;
   }
 
-    // useEffect che fa partire il timer
-    useEffect(() => {
-        let interval = null;
-        
-        if (isRunning) {
-            interval = setInterval(() => {
-                setSeconds(prevSeconds => {
-                    if (timerMode === 'countdown' && prevSeconds <= 0) {
-                        setIsRunning(false);
-                        return 0;
-                    }
-                    return timerMode === 'countdown' ? prevSeconds - 1 : prevSeconds + 1;
-                });
-            }, 1000);
-        } else {
-            clearInterval(interval);
-        }
-        
-        return () => clearInterval(interval);
-    }, [isRunning, timerMode]);
+  // useEffect che fa partire il timer
+  useEffect(() => {
+    let interval = null;
 
-    // Quando cambi modalità, resetta il timer
-    useEffect(() => {
-        if (timerMode === 'countdown' && hasTimer) {
-            setSeconds(totalMinutes * 60);
-        } else {
-            setSeconds(0);
-        }
-        setIsRunning(false);
-    }, [timerMode, totalMinutes, hasTimer]);
+    if (isRunning) {
+      interval = setInterval(() => {
+        setSeconds((prevSeconds) => {
+          if (timerMode === "countdown" && prevSeconds <= 0) {
+            setIsRunning(false);
+            return 0;
+          }
+          return timerMode === "countdown" ? prevSeconds - 1 : prevSeconds + 1;
+        });
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
 
-    /*
+    return () => clearInterval(interval);
+  }, [isRunning, timerMode]);
+
+  // Quando cambi modalità, resetta il timer
+  useEffect(() => {
+    if (timerMode === "countdown" && hasTimer) {
+      setSeconds(totalMinutes * 60);
+    } else {
+      setSeconds(0);
+    }
+    setIsRunning(false);
+  }, [timerMode, totalMinutes, hasTimer]);
+
+  /*
     
     //////////// Lun 8 12 25 \\\\\\\\\\\\
     function formatTime(totalSeconds) {
@@ -74,112 +73,108 @@ function RecipeTimer({ totalMinutes }) {
         return `${String(mins).padStart(2,'0')}:${String(secs).padStart(2,'0')}`;
     } */
 
-    const handlePlayPause = () => {
-        setIsRunning(!isRunning);
-    };
+  const handlePlayPause = () => {
+    setIsRunning(!isRunning);
+  };
 
-    const handleReset = () => {
-        setIsRunning(false);
-        if (timerMode === 'countdown' && hasTimer) {
-            setSeconds(totalMinutes * 60);
-        } else {
-            setSeconds(0);
-        }
-    };
+  const handleReset = () => {
+    setIsRunning(false);
+    if (timerMode === "countdown" && hasTimer) {
+      setSeconds(totalMinutes * 60);
+    } else {
+      setSeconds(0);
+    }
+  };
 
-    return (
-        <div className="recipe-timer-container">
-            {!isExpanded ? (
-                <div className="timer-compact">
-                    <h6 className="timer-header">Apri il timer:</h6>
-                    <button
-                        className="timer-icon"
-                        onClick={() => setIsExpanded(true)}
-                    >
-                        <img 
-                        src="/img/stopwatch-icon.png"
-                        alt="timer"
-                        className="timer-icon"
-                    />
-                    </button>
-                    
-                    {/* <span className="time-display">
+  return (
+    <div className="recipe-timer-container">
+      {!isExpanded ? (
+        <div className="timer-compact">
+          
+          <button className="timer-icon" onClick={() => setIsExpanded(true)}>
+            <img
+              src="/img/stopwatch-icon.png"
+              alt="timer"
+              className="open-timer-button"
+            />
+            <h6 className="timer-header">Apri il timer:</h6>
+          </button>
+
+          {/* <span className="time-display">
                         {formatTime(seconds)}
                     </span> */}
-                    
-                </div>
-            ) : (
-                <div className="extended-timer-header">
-                     <h6 className="timer-title">Timer Ricetta</h6>
-                    
-                    
-                   
-
-                    <button 
-                        className="timer-collapse-button"
-                        onClick={() => setIsExpanded(false)}
-                    >
-                        Nascondi timer <img 
-                        src="/img/stopwatch-icon.png"
-                        alt="timer"
-                        className="timer-icon"
-                    />
-                    </button>
-                    
-                    <div className="timer-mode-toggle">
-                        {hasTimer && (
-                            <button 
-                                onClick={() => setTimerMode('countdown')}
-                                className={timerMode === 'countdown' ? 'active' : ''}
-                            >
-                            <img 
-                                src="/img/start-stopwatch-icon.png"
-                                alt="timer"
-                                className="timer-icon"
-                            />
-                            Countdown ({totalMinutes} min)
-                            </button>
-                        )}
-                        <button 
-                            onClick={() => setTimerMode('stopwatch')}
-                            className={timerMode === 'stopwatch' ? 'active' : ''}
-                        >
-                            <img 
-                                src="/img/stopwatch.png"
-                                alt="timer"
-                                className="timer-icon"
-                            />Cronometro
-                        </button>
-                    </div>
-                    
-                    <div className="time-display-large">
-                        {formatTime(seconds)}
-                    </div>
-                    
-                    <div className="timer-controls">
-                    <button onClick={handlePlayPause} className="play-pause-button">
-                            <img 
-                                src={isRunning ? "/img/pause-timer-icon.png" : "/img/start-timer-icon.png"} 
-                                alt={isRunning ? "Pausa" : "Avvia"} 
-                                className="timer-icon"
-                            />
-                            <span>{isRunning ? 'Pausa' : 'Avvia'}</span>
-                        </button>
-                        <button onClick={handleReset}>
-                        <img 
-                                src="/img/timer-reset-icon.png"
-                                alt="timer"
-                                className="timer-icon"
-                            />
-                             Reset
-                        </button>
-                    </div>
-                    
-                    
-                </div>
-            )}
         </div>
-    );
+      ) : (
+        <div className="extended-timer-header">
+          <h6 className="timer-title">Timer Ricetta</h6>
+
+          <button
+            className="timer-collapse-button"
+            onClick={() => setIsExpanded(false)}
+          >
+            Nascondi timer{" "}
+            <img
+              src="/img/stopwatch-icon.png"
+              alt="timer"
+              className="timer-icon"
+            />
+          </button>
+
+          <div className="timer-mode-toggle">
+            {hasTimer && (
+              <button
+                onClick={() => setTimerMode("countdown")}
+                className={timerMode === "countdown" ? "active" : ""}
+              >
+                <img
+                  src="/img/start-stopwatch-icon.png"
+                  alt="timer"
+                  className="timer-icon"
+                />
+                Countdown ({totalMinutes} min)
+              </button>
+            )}
+            <button
+              onClick={() => setTimerMode("stopwatch")}
+              className={timerMode === "stopwatch" ? "active" : ""}
+            >
+              <img
+                src="/img/stopwatch.png"
+                alt="timer"
+                className="timer-icon"
+              />
+              Cronometro
+            </button>
+          </div>
+
+          <div className="time-display-large">{formatTime(seconds)}</div>
+
+          <div className="timer-controls">
+            <button onClick={handlePlayPause} className="play-pause-button">
+              <img
+                src={
+                  isRunning
+                    ? "/img/pause-timer-icon.png"
+                    : "/img/start-timer-icon.png"
+                }
+                alt={isRunning ? "Pausa" : "Avvia"}
+                className="timer-icon"
+              />
+              <span>{isRunning ? "Pausa" : "Avvia"}</span>
+            </button>
+            <button onClick={handleReset}>
+              <img
+                src="/img/timer-reset-icon.png"
+                alt="timer"
+                className="timer-icon"
+              />
+              Reset
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default RecipeTimer;
--- src/pages/recipeDetails.jsx
+++ src/pages/recipeDetails.jsx
@@ -1,102 +1,135 @@
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { getRecipeDetails } from "../services/api";
-import { getCachedRecipeDetails } from "../services/apiCache";
-import RecipeTimer from '../components/RecipeTimer';
+  import { useState, useEffect } from "react";
+  import { useParams } from "react-router-dom";
+  import { getRecipeDetails } from "../services/api";
+  import { getCachedRecipeDetails } from "../services/apiCache";
+  import RecipeTimer from "../components/RecipeTimer";
 
-export function RecipeDetails() {
-  const { id } = useParams();
-  const [recipe, setRecipe] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-  
-  const [showIngredients, setShowIngredients] = useState(false);
-  const [showInstructions, setShowInstructions] = useState(false);
-  const [showConservation, setShowConservation] = useState(false);
+  export function RecipeDetails() {
+    const { id } = useParams();
+    const [recipe, setRecipe] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-  useEffect(() => {
-    setIsLoading(true);
-    getCachedRecipeDetails(getRecipeDetails, id)
-      .then((data) => {
-        setRecipe(data);
-        setIsLoading(false);
-      })
-      .catch(() => {
-        setError("Errore nel caricamento");
-        setIsLoading(false);
-      });
-  }, [id]);
+    const [showIngredients, setShowIngredients] = useState(false);
+    const [showInstructions, setShowInstructions] = useState(false);
+    const [showConservation, setShowConservation] = useState(false);
 
-  if (isLoading) return <div>Caricamento ricetta...</div>;
-  if (error) return <div>{error}</div>;
+    useEffect(() => {
+      setIsLoading(true);
+      getCachedRecipeDetails(getRecipeDetails, id)
+        .then((data) => {
+          setRecipe(data);
+          setIsLoading(false);
+        })
+        .catch(() => {
+          setError("Errore nel caricamento");
+          setIsLoading(false);
+        });
+    }, [id]);
 
-  return (
-    <div className="recipe-detail-container">
-      <div className="recipe-detail-titol">
-        <h2>{recipe.title}</h2>
-      </div>
+    if (isLoading) return <div>Caricamento ricetta...</div>;
+    if (error) return <div>{error}</div>;
 
-      <img
-        className="recipe-detail-immagine"
-        src={recipe.image}
-        alt="foto della ricetta"
-      />
+    return (
+      <div className="recipe-detail-container">
+        <div className="recipe-detail-titol">
+          <h2>{recipe.title}</h2>
+        </div>
 
-      <div
-        className="description"
-        dangerouslySetInnerHTML={{
-          __html: recipe.summary || "Nessuna descrizione disponibile.",
-        }}
-      />
+        <img
+          className="recipe-detail-immagine"
+          src={recipe.image}
+          alt="foto della ricetta"
+        />
 
-      <div className="container-bottoni">
-        <button onClick={() => setShowIngredients(!showIngredients)}>
-          {showIngredients ? "Nascondi Ingredienti" : "Lista Ingredienti"}
-        </button>
-        <button onClick={() => setShowInstructions(!showInstructions)}>
-          {showInstructions ? "Nascondi Preparazione" : "Mostra Preparazione"}
-        </button>
-        <button onClick={() => setShowConservation(!showConservation)}>
-          {showConservation ? "Nascondi Conservazione" : "Conservazione"}
-        </button>
-      </div>
-
-      {/* Ingredienti */}
-      <div className={`collapsible ${showIngredients ? "open" : ""}`}>
-        <ul className="container-ingredienti">
-          {recipe.extendedIngredients?.map((item) => (
-            <li className="card-ingrediente" key={item.id}>
-              <img
-                className="immagine-ingrediente"
-                src={`https://spoonacular.com/cdn/ingredients_100x100/${item.image}`}
-                alt={item.name}
-              />
-              {item.original}
-            </li>
-          ))}
-        </ul>
-        <div className="scroll-hint">Scorri →</div>
-      </div>
-
-      {/* Preparazione + Timer */}
-      <div className={`collapsible ${showInstructions ? "open" : ""}`}>
-        <RecipeTimer totalMinutes={recipe.readyInMinutes} />
         <div
-          className="preparazione"
+          className="description"
           dangerouslySetInnerHTML={{
-            __html: recipe.instructions || "N/A",
+            __html: recipe.summary || "Nessuna descrizione disponibile.",
           }}
         />
-      </div>
 
-      {/* Conservazione */}
-      <div className={`collapsible ${showConservation ? "open" : ""}`}>
-        <div className="conservazione">
-          <p>
-            Conserva in frigorifero per massimo 2 giorni in contenitore ermetico.
-          </p>
+        <h6 className="section-header">Menu:</h6>
+        <div className="container-bottoni">
+        <button
+  className={`menu-button ${showIngredients ? "active" : ""}`}
+  onClick={() => setShowIngredients(!showIngredients)}
+>
+  <img
+    className="menu-icon"
+    src="/img/ingredients-icon.png"
+    alt="icona ingredienti"
+  />
+  <span>{showIngredients ? "Nascondi" : "Ingredienti"}</span>
+</button>
+
+<button
+  className={`menu-button ${showInstructions ? "active" : ""}`}
+  onClick={() => setShowInstructions(!showInstructions)}
+>
+  <img
+    className="menu-icon"
+    src="/img/instructions-icon.png"
+    alt="icona preparazione"
+  />
+  <span>{showInstructions ? "Nascondi" : "Preparazione"}</span>
+</button>
+
+<button
+  className={`menu-button ${showConservation ? "active" : ""}`}
+  onClick={() => setShowConservation(!showConservation)}
+>
+  <img
+    className="menu-icon"
+    src="/img/preserve-food-icon.png"
+    alt="icona conservazione"
+  />
+  <span>{showConservation ? "Nascondi" : "Conservazione"}</span>
+</button>
+
+        </div>
+
+        {/* Ingredienti */}
+        <div className={`collapsible ${showIngredients ? "open" : ""}`}>
+          <h6 className="section-header">Ingredienti:</h6>
+          <ul className="container-ingredienti">
+            {recipe.extendedIngredients?.map((item) => (
+              <li className="card-ingrediente" key={item.id}>
+                <img
+                  className="immagine-ingrediente"
+                  src={`https://spoonacular.com/cdn/ingredients_100x100/${item.image}`}
+                  alt={item.name}
+                />
+                {item.original}
+              </li>
+            ))}
+          </ul>
+          <div className="scroll-hint">Scorri →</div>
+        </div>
+
+        {/* Preparazione + Timer */}
+        <div className={`collapsible ${showInstructions ? "open" : ""}`}>
+          <h6 className="section-header">Come lo preparo:</h6>
+
+          <div
+            className="preparazione"
+            dangerouslySetInnerHTML={{
+              __html: recipe.instructions || "N/A",
+            }}
+          />
+          <RecipeTimer totalMinutes={recipe.readyInMinutes} />
+        </div>
+
+        {/* Conservazione */}
+        <div className={`collapsible ${showConservation ? "open" : ""}`}>
+          <h6 className="section-header">Come conservare:</h6>
+          <div className="conservazione">
+            <p>
+              Conserva in frigorifero per massimo 2 giorni in contenitore
+              ermetico.
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
